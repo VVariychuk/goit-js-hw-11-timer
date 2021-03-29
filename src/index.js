d0b8af5ad671e375de1b3e794b52c01edf295c87
varenik <v.variychuk.nts@gmail.com>
2021-03-29 21:30:33 +0300

    creating to Easter timer

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import './styles.css';
+
+class CountdownTimer {
+
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.refs = {
+      days: document.querySelector(`${this.selector} [data-value="days"]`),
+      hours: document.querySelector(`${this.selector} [data-value="hours"]`),
+      mins: document.querySelector(`${this.selector} [data-value="mins"]`),
+      secs: document.querySelector(`${this.selector} [data-value="secs"]`),
+    };
+    this.getTimeToTarget();
+  }
+  getTimeToTarget() {
+    setInterval(() => {    
+      const currentTime = Date.now();
+      
+      const time = this.targetDate - currentTime;  
+      const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+      const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+      this.refs.days.textContent = days;
+      this.refs.hours.textContent = hours;
+      this.refs.mins.textContent = mins;
+      this.refs.secs.textContent = secs;
+    }, 1000);
+  }
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('May 02, 2021'),
+});
