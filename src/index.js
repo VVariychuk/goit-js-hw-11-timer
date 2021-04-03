f4cb5390eaa31705200ead3ccc8cea952ce23979
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-04-03 17:41:07 +0300

    вынес работы с ДОМ в отдельную функцию

--- src/index.js
+++ src/index.js
@@ -11,31 +11,36 @@ class CountdownTimer {
       mins: document.querySelector(`${this.selector} [data-value="mins"]`),
       secs: document.querySelector(`${this.selector} [data-value="secs"]`),
     };
-    this.getTimeToTarget();
-  }
+    this.getTimeToTarget()
+    this.setTimeToTarget()
+    }
   getTimeToTarget() {
     setInterval(() => {    
-      const currentTime = Date.now();
+      const currentTime = Date.now()
       
-      const time = this.targetDate - currentTime;  
-      const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-      const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-      const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-      this.refs.days.textContent = days;
-      this.refs.hours.textContent = hours;
-      this.refs.mins.textContent = mins;
-      this.refs.secs.textContent = secs;
+      const time = this.targetDate - currentTime
+      const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)))
+      const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+      const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)))
+      const secs = pad(Math.floor((time % (1000 * 60)) / 1000))
+      
+      this.setTimeToTarget(days, hours, mins, secs)
     }, 1000);
   }
+  setTimeToTarget(days, hours, mins, secs) {
+      this.refs.days.textContent = days
+      this.refs.hours.textContent = hours
+      this.refs.mins.textContent = mins
+      this.refs.secs.textContent = secs
+   }
+
 }
 
 function pad(value) {
-  return String(value).padStart(2, '0');
+  return String(value).padStart(2, '0')
 }
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('May 02, 2021'),
-});
+})
